0d0dbe1aff3dc88493bb072bfba1b3e6f311fdbf
OS <anhtuan.051998@gmail.com>
2018-09-21 03:59:55 +0700

    asaad

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -4,3 +4,149 @@ public class Task3 {
 }
 
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
+class Book{
+    //TODO: khai bao thuoc tinh cho class Books
+    String name =null;
+    String author =null;
+    int release_year = -1;
+
+    //TODO: getter, setter cho cac thuoc tinh
+    public String getName(){
+        try{
+            if(name!= null){
+                return name;
+            }
+            else {
+                System.out.println("Set name first.");
+                return null;
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public void setName(String n){
+        try{
+            name = n;
+        }
+        catch(NullPointerException | IllegalArgumentException e){
+            System.out.println(e);
+            System.out.println("Re-enter argument.");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public String getAuthor(){
+        try{
+            if(author!= null){
+                return author;
+            }
+            else {
+                System.out.println("Set author first.");
+                return null;
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public void setAuthor(String n){
+        try{
+            author = n;
+        }
+        catch(NullPointerException | IllegalArgumentException e){
+            System.out.println(e);
+            System.out.println("Re-enter argument.");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public int getReleaseYear(){
+        try{
+            if(release_year >= 0){
+                return release_year;
+            }
+            else {
+                System.out.println("Set release year first.");
+                return -1;
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return -1;
+        }
+    }
+
+    public void setID(int n){
+        try{
+            release_year = n;
+        }
+        catch(NullPointerException | IllegalArgumentException e){
+            System.out.println(e);
+            System.out.println("Re-enter argument");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    //TODO: Tao mang books
+    Book books[] = new Book[100];
+
+    //TODO: In danh muc sach cua tac gia X
+    public void printBookof(String author){
+        for(Book book:books) {
+            if(book.getAuthor().equals(author)) {
+                System.out.println(book.getName());
+            }
+        }
+    }
+
+    //TODO: In danh muc sach xuat ban sau nam X
+    public void printBookafter(int year){
+        for(Book book:books) {
+            if(book.getReleaseYear() > year) {
+                System.out.println("Name: " + book.getName() +"\nAuthor: " + book.getAuthor());
+            }
+        }
+    }
+
+    //TODO: Xoa sach xuat ban truoc nam X ra khoi mang
+    public void removeBookBefore(int year){
+        for(int i=0;i<books.length;i++) {
+            if(books[i] == null)
+                break;
+            if(books[i].getReleaseYear() < year){
+                for(int j=i ;j<books.length -1;j++){
+                    books[j] = books[j+1];
+                }
+                i--;
+            }
+        }
+    }
+
+}
+
+class Friend{
+    //TODO: khai bao thuoc tinh cho class Books
+    String name;
+    int age;
+    String email;
+
+    //TODO: getter, setter cho cac thuoc tinh
+}
+class Phone{
+    //TODO: khai bao thuoc tinh cho class Books
+    String brand;
+    int price;
+    int screensize;
+
+    //TODO: getter, setter cho cac thuoc tinh
+}
