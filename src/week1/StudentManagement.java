65a5c68251023b7d632b42eac7f34dea15758267
OS <anhtuan.051998@gmail.com>
2018-09-21 14:00:29 +0700

    gui email

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -85,18 +85,15 @@ public class StudentManagement {
         //TODO:test constructor 1: Student()
         System.out.println("-----------constructor1------------");
         students[1] = new Student();
-        System.out.println(students[1].getName());
         System.out.println(students[1].getInfo());
 
         //TODO: test constructor 2:
         System.out.println("-----------constructor2------------");
         students[2] = new Student("Nguyen Anh Tuan","16021213","anhtuan.051998@gmail.com");
-        System.out.println(students[2].getName());
         System.out.println(students[2].getInfo());
         //TODO:test constructor 3: Student(Student s)
         System.out.println("-----------constructor3------------");
         students[3] = new Student(students[0]);
-        System.out.println(students[3].getName());
         System.out.println(students[3].getInfo());
         students[4] = new Student();
         students[4].setGroup("INT22042");
@@ -106,24 +103,18 @@ public class StudentManagement {
         students[6].setGroup("INT22042");
         System.out.println("-----------test sameGroup------------");
         StudentManagement test = new StudentManagement();
-        System.out.println("student 4 va student 5 cung lop: " + test.sameGroup(students[4],students[5]));
-        System.out.println("student 4 va student 6 cung lop: " + test.sameGroup(students[4],students[6]));
-        System.out.println("student 5 va student 6 cung lop: " + test.sameGroup(students[5],students[6]));
+//        System.out.println("student 4 va student 5 cung lop: " + test.sameGroup(students[4],students[5]));
+//        System.out.println("student 4 va student 6 cung lop: " + test.sameGroup(students[4],students[6]));
+//        System.out.println("student 5 va student 6 cung lop: " + test.sameGroup(students[5],students[6]));
 
         //students[1].setGroup("INT22042");
-        /*:TODO:Test method removeStudent
-        StudentManagement remove = new StudentManagement();
-        remove.removeStudent("16021213");
-        //remove.removeStudent("000");*/
-        System.out.println("Before studentsByGroup");
-        for(Student s:students){
-            if(s == null)
-                break;
-            System.out.println(s.getInfo());
-        }
-        System.out.println("--------------------------------------");
-        System.out.println("After studentsByGroup");
+        //:TODO:Test method removeStudent
         StudentManagement print = new StudentManagement();
         print.studentsByGroup();
+        System.out.println("============================");
+        StudentManagement remove = new StudentManagement();
+        remove.removeStudent("16021213");
+        print.studentsByGroup();
+
     }
 }
--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -1,5 +1,11 @@
 package week2.task3;
 
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
 public class Task3 {
 }
 
@@ -136,11 +142,164 @@ class Book{
 
 class Friend{
     //TODO: khai bao thuoc tinh cho class Books
-    String name;
-    int age;
-    String email;
+    String name = null;
+    String number= null;
+    String email = null;
 
+    Friend friends[] = new Friend[1000];
     //TODO: getter, setter cho cac thuoc tinh
+    public String getName(){
+        try{
+            if(name!= null){
+                return name;
+            }
+            else {
+                System.out.println("Set name first.");
+                return null;
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public String getEmail(){
+        try{
+            if(email!= null){
+                return email;
+            }
+            else {
+                System.out.println("Set email first.");
+                return null;
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public String getNumber(){
+        try{
+            if(number!=null){
+                return number;
+            }
+            else {
+                System.out.println("Set number first.");
+                return null;
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public void setName(String n){
+        try{
+            name = n;
+        }
+        catch(NullPointerException | IllegalArgumentException e){
+            System.out.println(e);
+            System.out.println("Re-enter argument.");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void setEmail(String n){
+        try{
+            email = n;
+        }
+        catch(NullPointerException | IllegalArgumentException e){
+            System.out.println(e);
+            System.out.println("Re-enter argument.");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void setNumber(String n){
+        try{
+            number = n;
+        }
+        catch(NullPointerException | IllegalArgumentException e){
+            System.out.println(e);
+            System.out.println("Re-enter argument.");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    //TODO: In contact(number + email) cua ban ten A
+    public void contact(String name){
+        try{
+            if(this.name.equals(name)){
+                System.out.println("Email: " + this.email + "\nNumber: " + this.number);
+            }
+        }
+        catch(NullPointerException | IllegalArgumentException e){
+            System.out.println(e);
+            System.out.println("Re-enter argument.");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    //TODO: Unfriend ban A
+    public void unfriend(Friend A){
+        try {
+            for(int i=0;i<friends.length;i++) {
+                if(friends[i] == null)
+                    break;
+                if(friends[i] == A){
+                    for(int j=i ;j<friends.length -1;j++){
+                        friends[j] = friends[j+1];
+                    }
+                    i--;
+                }
+            }
+
+        }
+        catch(Exception e){
+            System.out.println("Exception: " +e);
+        }
+
+    }
+
+    //TODO: Gui tin nhan "I love you" cho ban A
+    public void confess(Friend A){
+        try {
+            // Recipient's email ID needs to be mentioned.
+            String to = "abcd@gmail.com";
+
+            // Sender's email ID needs to be mentioned
+            String from = "web@gmail.com";
+
+            // Assuming you are sending email from localhost
+            String host = "localhost";
+
+            // Get system properties
+            Properties properties = System.getProperties();
+
+            // Setup mail server
+            properties.setProperty("mail.smtp.host", host);
+
+            // Get the default Session object.
+            Session session = Session.getDefaultInstance(properties);
+
+
+        }
+        catch(MessagingException mex){
+            mex.printStackTrace();
+        }
+    }
+
 }
 class Phone{
     //TODO: khai bao thuoc tinh cho class Books
