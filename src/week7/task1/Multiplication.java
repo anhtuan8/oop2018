ec979d55cbaa1bb2a42bd12773b03e47c164a6f2
anhtuan8 <43262400+anhtuan8@users.noreply.github.com>
2018-11-02 14:21:57 +0700

    adsd

--- src/week7/task1/Multiplication.java
+++ src/week7/task1/Multiplication.java
@@ -1,22 +1,9 @@
 package week7.task1;
 
 public class Multiplication extends BinaryExpression {
-    private Expression left;
-    private Expression right;
-
     public Multiplication(Expression left,Expression right){
         this.left = left;
         this.right = right;
-    }
-
-    @Override
-    public Expression left(){
-
-        return this.left;
-    }
-    @Override
-    public Expression right(){
-        return this.right;
     }
     @Override
     public String toString(){
