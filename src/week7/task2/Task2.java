e92d5291e28c9d65de69f557ca2fbfcb0d401ec2
anhtuan8 <43262400+anhtuan8@users.noreply.github.com>
2018-11-02 14:14:39 +0700

    adsd

--- src/week7/task2/Task2.java
+++ src/week7/task2/Task2.java
@@ -1,7 +1,6 @@
 package week7.task2;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Task2 {
@@ -35,13 +34,17 @@ public class Task2 {
     }
 
     public void exc4()throws ClassCastException{
-        Object a = "abcd";
-        System.out.println((int) a);
+        Object a = 42;
+        String s = (String) a;
+        System.out.println(s);
     }
 
     public void exc5() throws IOException{
-        Scanner scanner = new Scanner(new File(".\\E_V.ddd"));
-
+        Scanner scanner = new Scanner(new File(".\\abcd.txt"));
+        while(scanner.hasNextInt()) {
+            int s = scanner.nextInt();
+            System.out.println(s);
+        }
     }
 
     public void exc6() throws FileNotFoundException {
@@ -87,7 +90,7 @@ public class Task2 {
         try{
             tester.exc5();
         }catch (IOException e){
-            System.out.println(e);
+            System.out.println("IOException");
         }
 
         try{
